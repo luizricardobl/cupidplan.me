d123a17a59f8e6dc3652c98dd1818b2ccaad7ed0
luizricardobl <luizricardobl@hotmail.com>
2025-03-20 21:03:47 -0400

    Work step 1
    Test

--- cupidplan-frontend/src/pages/Matches.js
+++ cupidplan-frontend/src/pages/Matches.js
@@ -1,10 +1,13 @@
 import React from "react";
+import TinderCard from "react-tinder-card";
 
 const Matches = () => {
   return (
     <div>
       <h1>Matches Page</h1>
       <p>Here you will see potential matches based on preferences.</p>
+        <p>Cards here</p>
+        <TinderCard>eafadsfa</TinderCard>
     </div>
   );
 };
